1a5d07be6a7e2d043ef4afc9d4dbb1444d81d23a
Chudasama vanraj <50484070+vanrajchudasama@users.noreply.github.com>
2021-01-11 19:56:05 -0800

    Update settings.py

--- OGMS/settings.py
+++ OGMS/settings.py
@@ -133,6 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'vchudasma1997@gmail.com'
-EMAIL_HOST_PASSWORD = 'tkmvilracrsozhje'
+EMAIL_HOST_USER = 'XYZ@gmail.com'
+EMAIL_HOST_PASSWORD = 'Password'
 
